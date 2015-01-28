8ae95cf6b662b18df8461e5a18aff9022b710eee
Nick VanMeter <nicholas.vanmeter@mindbodyonline.com>
2015-01-28 15:28:35 -0800

    Script cleanup and commenting.

--- js/script.js
+++ js/script.js
@@ -1,37 +1,13 @@
 (function(model) {
 	
-	model.Game = function() {
+	var Game = function() {
 		var self = this;
 
 		self._getChar = function() {
 			return self.playCount % 2 ? "X" : "O";
 		};
 		
-		self.X = 0;
-		self.O = 0;
-		self.Tie = 0;
-
-		self.HandleClick = function(e) {
-			var character = self._getChar();
-			var $target = $(e.target),
-				data = $target.data(),
-				x = data.x,
-				y = data.y,
-				point = self.board[y][x];
-
-			if(!point) {
-				self.board[y][x] = character;
-				$target.text(character).addClass(character);
-			}
-
-			if(self.playCount >= 4) {
-				self.CheckForWinner();
-			} else {
-				self.NextPlay();
-			}
-		};
-
-		self.BuildBoard = function() {
+		self._buildBoard = function() {
 			var $container = $("#board").empty();
 			for(var y = 0; y < 3; y++) {
 				var $row = $("<tr/>");
@@ -43,79 +19,121 @@
 			}
 		};
 
-		self.NextPlay = function() {
+		//Setup Stats
+		self.X = 0;
+		self.O = 0;
+		self.Tie = 0;
+
+		self.handleClick = function(e) {
+			var character = self._getChar();
+			var $target = $(e.target),
+				data = $target.data(),
+				x = data.x,
+				y = data.y,
+				point = self.board[y][x];
+
+			if(point == "") {
+				//clicked td is empty, go ahead and assign it a character.
+				self.board[y][x] = character;
+				$target.text(character).addClass(character);
+			}
+			if(self.playCount < 4) {
+				//Not possible to have a winner yet, keep playing!
+				return self.nextPlay();
+			}
+			if(self.playCount >= 4) {
+				if(self.isWinner()) {
+					//Winner exists!
+					self.handleWin();
+				} else if(self.playCount == 8) {
+					//Game Over!
+					self.handleTie();
+				} else {
+					//Keep playing!
+					self.nextPlay();
+				}
+			}
+		};
+
+		self.handleWin = function() {
+			var character = self._getChar();
+
+			alert(character + " wins!");
+			self[character]++
+			self.reset();
+		};
+
+		self.handleTie = function() {
+			alert("no one wins :(");
+			self.Tie++;
+			self.reset();
+		};
+
+		self.nextPlay = function() {
 			self.playCount++;
 		};
 
-		self.CheckForWinner = function() {
-			var player = self._getChar();
+		self.isWinner = function() {
+			var player = self._getChar(),
+				i,
+				center = self.board[1][1];
+
 			// rows
-			var win = false;
-			self.board.forEach(function(row) {
+			for(i = 0;i<3;i++) {
+				var row = self.board[i];
 				if(row.every(function(cell) {
 					return cell == player
 				})) {
 					//left-to-right win!
-					win = true;
+					return true;
 				};
-			})
+			}
 
 			//columns
-			for(var i = 0;i<3; i++) {
+			for(i = 0;i<3; i++) {
 				if(self.board[0][i] == player &&
 					self.board[1][i] == player &&
 					self.board[2][i] == player) {
 
 					//top to bottom win!
-					win = true;
+					return true;
 				}
 			}
 
 			//diagonal
-			var center = self.board[1][1];
 			if(center == player) {
 				if(self.board[0][0] == player && self.board[2][2] == player) {
 					//top-left to bottom-right win!
-					win = true;
+					return true;
 				}
 				if(self.board[0][2] == player && self.board[2][0] == player) {
 					//top-right to bottom-left win!
-					win = true;
+					return true;
 				}
 			}
-
-			if(win) {
-				alert(player + " wins!");
-				self[player]++
-				self.Reset();
-			} else if(self.playCount == 8) {
-				alert("no one wins :(");
-				self.Tie++;
-				self.Reset();
-			} else {
-				self.NextPlay();
-			}
+			
+			return false;
 		};
 
-		self.Reset = function() {
+		self.reset = function() {
 			self.playCount = 0;
 			self.board = [
 				["", "" ,"" ],
 				["" ,"" ,"" ],
 				["" ,"" ,"" ]
 			];
-			self.BuildBoard();
-			self.UpdateStats();
+			self.updateStats();
+			self._buildBoard();
 		};
 
-		self.UpdateStats = function(){
+		self.updateStats = function(){
 			$(".x-count").text(self.X);
 			$(".o-count").text(self.O);
 			$(".tie-count").text(self.Tie);
 		};
 
 	    self.init = function() {
-	    	self.Reset();
+	    	self.reset();
 	    };
 	    
 	    self.init();
@@ -123,8 +141,11 @@
 		return self;
 	}
 
-	$(document).ready(function() {
-		window.Game = new model.Game();
-		$("#board").on("click","td",Game.HandleClick)
-	});
-})({});
+	window.Game = new Game();
+
+})();
+
+
+$(document).ready(function() {
+	$("#board").on("click","td",Game.handleClick)
+});
